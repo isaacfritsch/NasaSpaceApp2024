829c4178535fd9de2062dc89539746003c4e0137
isaacfritsch <fritsch.isaac@yahoo.com.br>
2024-10-05 17:47:45 -0300

    primeiros htmls

--- mapas/urls.py
+++ mapas/urls.py
@@ -4,8 +4,8 @@ from mapas import views
 
 
 urlpatterns = [
-    path('', views.mapas, name='mapas'),
-    # path('mapas1/', views.mapas1, name='mapas1'),
+    path('', views.home, name='home'),    
+    path('mapas', views.mapas1, name='mapas1'),
     path('mapas2/', views.mapas2, name='mapas2'),
     path('mapas3/', views.mapas3, name='mapas3'),
     path('mapas4/', views.mapas4, name='mapas4'),    
--- mapas/views.py
+++ mapas/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 
 
-def mapas(request): 
-    return render(request, 'mapas/mapas.html')
+def home(request): 
+    return render(request, 'mapas/home.html')
+
+def mapas1(request): 
+    return render(request, 'mapas/mapas1.html')
 def mapas2(request): 
     return render(request, 'mapas/mapas2.html')
 def mapas3(request): 
     return render(request, 'mapas/mapas3.html')
 def mapas4(request): 
     return render(request, 'mapas/mapas4.html')
+def mapas5(request): 
+    return render(request, 'mapas/mapas5.html')
