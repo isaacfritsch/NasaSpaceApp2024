5bcfd158288bf4e0ae86aaa3a1a93064cd903dec
isaacfritsch <fritsch.isaac@yahoo.com.br>
2024-10-05 20:03:07 -0300

    html

--- mapas/urls.py
+++ mapas/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('mapas', views.mapas1, name='mapas1'),
     path('mapas2/', views.mapas2, name='mapas2'),
     path('mapas3/', views.mapas3, name='mapas3'),
-    path('mapas4/', views.mapas4, name='mapas4'),    
+    path('mapas4/', views.mapas4, name='mapas4'), 
+    path('proxy/xeno-canto/', views.proxy_xeno_canto, name='proxy_xeno_canto'),  
 ]
--- mapas/views.py
+++ mapas/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.http import HttpResponse
+import requests
 
 
 def home(request): 
@@ -14,3 +17,15 @@ def mapas4(request):
     return render(request, 'mapas/mapas4.html')
 def mapas5(request): 
     return render(request, 'mapas/mapas5.html')
+
+
+def proxy_xeno_canto(request):
+    # URL da API do Xeno-canto
+    url = 'https://www.xeno-canto.org/api/2/recordings?query=cnt:Brazil'
+    
+    # Faz a requisição para a API do Xeno-canto
+    response = requests.get(url)
+    
+    # Retorna a resposta JSON da API para o frontend
+    return JsonResponse(response.json())
+
